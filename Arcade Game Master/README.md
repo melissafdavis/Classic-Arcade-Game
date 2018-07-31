cbcb1a1a111d43d3b1a859ebce3daedfeedcf914
nevernotlearning <37967579+nevernotlearning@users.noreply.github.com>
2018-07-31 08:27:21 -0500

    Initial upload of files

---
+++ Arcade Game Master/README.md
@@ -0,0 +1,23 @@
+
+This is my remix of the classic frogger game, submitted for Udacity's front-end web development nanodegree.
+
+#To Install
+1. Download or clone the repository to your computer.
+2. Open the index html in your browser.
+
+#To Play
+Use the arrow keys to move the player:
+
+Up: up-arrow
+Down: down-arrow
+Left: left-arrow 
+Right: right-arrow
+
+The object of the game is to earn as many points as possible by getting to the water without colliding with the bugs. The player earns a point each time he/she makes it to the water. If you collide with the bugs, you lose and a modal will appear giving you the option to play again or leave the game.
+
+#Useful resources
+I am very grateful to the following developers for sharing their projects online. Their examples helped me to understand and get inspired for this project.
+
+http://fabiolavieyra.com/blog/arcade-game
+http://irene.marin.cat/udacity/project3/index.html
+https://medium.com/letsboot/classic-arcade-game-with-js-5687e4125169
---
+++ Arcade Game Master/js/app.js
@@ -0,0 +1,141 @@
+var points = 0;
+document.querySelector('.score').innerHTML = `Score : ${points}`;
+function resetPoints (){
+  points = 0;
+  document.querySelector('.score').innerHTML = `Score : ${points}`;
+}
+
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
+
+    // The following variables are used to determine the x and y axis and speed of the enemy
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+
+    // The image of the enemy of cockroach that is added to the playing field
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+
+    // Multiplies the speed by the dt parameter on the x axis
+    this.x += this.speed * dt;
+
+    // Once enemies are off the canvas, they reappear randomly with different speeds
+    if (this.x > 510) {
+        this.x = -50;
+        this.speed = 100 + Math.floor(Math.random() * 222);
+    };
+
+    // Checks for collisions between the player and the enemies
+    if (player.x < this.x + 80 &&
+        player.x + 80 > this.x &&
+        player.y < this.y + 60 &&
+        60 + player.y > this.y) {
+        player.x = 202;
+        player.y = 405;
+
+        swal({
+          title: "Oh no!",
+          text: "The bug got you",
+          icon: "error",
+          buttons: ['Bye bye', 'Play again'],
+          dangerMode: false,
+        })
+        .then((playAgain) => {
+          if (playAgain) {
+              resetPoints();
+
+  } else {
+    swal("Thanks for playing!");
+    resetPoints();
+  }
+});
+    };
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+var Player = function(x,y){
+  this.x = x;
+  this.y = y;
+  this.player = 'images/char-pink-girl.png';
+}
+// Player class requires an update() method
+Player.prototype.update = function (dt) {
+};
+
+// Player class requires a render() method
+Player.prototype.render = function (){
+    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+};
+
+// Player class requires a handleInput() method.
+Player.prototype.handleInput = function (keyPress) {
+
+    // Enables user on left arrow key to move left on the x axis by 102
+    // Also enables user not to go off the game tiles on the left side
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+
+    // Enables user on right arrow key to move right on the x axis by 102
+    // Also enables user not to go off the game tiles on the right side
+    if (keyPress == 'right' && this.x < 405) {
+        this.x += 102;
+    };
+
+    // Enables user on up arrow key to move upwards on the y axis by 83
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+
+    // Enables user on down arrow key to move downwards on the y axis by 83
+    // Also enables user not to go off the game tiles on the bottom side
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    };
+
+    // Once the user reaches the top of the page; the water, the user is
+    // Instantly reset to the starting position and award a point
+    if (this.y <  20) {
+        setTimeout(() => {
+            this.x = 202;
+            this.y = 405;
+        }, 100);
+        points ++;
+        document.querySelector('.score').innerHTML = `Score : ${points}`;
+    };
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+// Place the player object in a variable called player
+var enemyLocation = [63, 147, 230];
+
+enemyLocation.forEach(function (locationY) {
+    enemy = new Enemy(0, locationY, 200);
+    allEnemies.push(enemy);
+});
+
+var player = new Player(202, 405);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
